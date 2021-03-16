cbf1e46823180cd3ace497cb7114140a8f07578b
Invis1bleReborn <68089411+Invis1bleReborn@users.noreply.github.com>
2021-03-16 07:36:00 +0300

    Add ability to configure an interval, close #2.

--- src/main/scala/com/github/invis1ble/whatismyip/Config.scala
+++ src/main/scala/com/github/invis1ble/whatismyip/Config.scala
@@ -1,5 +1,9 @@
 package com.github.invis1ble.whatismyip
 
 import java.io.File
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
-final case class Config(file: Option[File] = None)
+final case class Config(
+  file: Option[File] = None,
+  interval: FiniteDuration = 1.minute,
+)
